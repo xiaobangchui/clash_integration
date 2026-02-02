3e17122e221375d8c00daf60db65d3bda7039c2b
卖女孩の小棒槌 <42337108+xiaobangchui@users.noreply.github.com>
2026-02-02 23:06:50 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -173,10 +173,6 @@ sniffer:
       ports: [80, 8080-8880]
     QUIC: 
       ports: [443, 8443]
-  # 强制部分域名重新嗅探
-  force-domain:
-    - '+.okx.com'
-    - '+.binance.com'
 
 dns:
   enable: true
@@ -192,13 +188,6 @@ dns:
     - 'clients*.google.com'
     - 'connectivitycheck.gstatic.com'
     - 'detectportal.firefox.com'
-	- '+.okx.com'
-    - '+.okex.com'
-    - '+.oklink.com'
-    - '+.binance.com'
-    - '+.bnbdaze.com'
-    - '+.metamask.io'
-    - '+.walletconnect.com'
     - '+.douyin.com'
     - '+.bytedance.com'
     - '+.baidu.com'
