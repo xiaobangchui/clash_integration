69dbe6c298b170f341236241d2dab99bc304aa91
卖女孩の小棒槌 <920888069@qq.com>
2026-04-10 00:13:07 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -123,6 +123,7 @@ export default {
     const jp = nodeNames.filter(n => /(JP|Japan|日|日本)/i.test(n));
     const sg = nodeNames.filter(n => /(SG|Singapore|狮城|新|新加坡)/i.test(n));
     const usa = nodeNames.filter(n => /(US|United|States|America|美|美国)/i.test(n));
+    const cflare = nodeNames.filter(n => /(CF官方优选)/i.test(n));
     const others = nodeNames.filter(n => !/(HK|Hong|Kong|港|香港|TW|Taiwan|台|台湾|JP|Japan|日|日本|SG|Singapore|狮城|新|新加坡|US|United|States|America|美|美国)/i.test(n));
 
     const usedGB = (summary.used / (1024 ** 3)).toFixed(1);
@@ -356,9 +357,8 @@ ${makeGroup(hk)}
     type: url-test
     url: https://www.google.com/generate_204
     interval: 600
-    tolerance: 100
-    lazy: true
-    filter: "CF官方优选"
+    proxies:
+${makeGroup(cflare)}
 
   - name: "🏮 Taiwan"
     type: url-test
